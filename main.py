fe9e6b3495f6406b1005ba31f86d663bd4af0b1a
ayswl <aiyas.aboobakar@gmail.com>
2018-11-01 06:53:18 +0800

    things

---
+++ main.py
@@ -0,0 +1,14 @@
+istep = _int(step)
+        if istep != step:
+            raise ValueError("non-integer step for randrange()")
+        if istep > 0:
+            n = (width + istep - 1) // istep
+        elif istep < 0:
+            n = (width + istep + 1) // istep
+        else:
+            raise ValueError("zero step for randrange()")
+
+        if n <= 0:
+            raise ValueError("empty range for randrange()")
+
+return istart + istep*self._randbelow(n)
